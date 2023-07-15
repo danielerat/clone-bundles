181131ed51098c133440a10cd5492a1081a5c7bc
danielerat <davidodo2015@gmail.com>
2023-07-16 00:36:44 +0200

    First Site Clone

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,11 @@ module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {
+      backgroundImage: {
+        "hero-pattern": "url('../img/hero.svg')",
+      },
       colors: {
+        primary: "#c30a17",
       },
     },
   },
